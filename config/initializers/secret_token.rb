ae2b046641a77d77dee7ce09799de1dcd823497b
Adhiraj Rankhambe <adhiraj_rankhambe@external.mckinsey.com>
2013-04-19 14:52:45 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreezingIronman::Application.config.secret_token = '4666680e5fe31eceac86bd13df093ff5ea33f276d9d157a480efef22cba1363d2700ada8c5e3dae85978a860df750cd895617349639c40b384eeee4977445889'
